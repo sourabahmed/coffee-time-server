ff95337bcc3b1a093e37c1c376960cbfa7bb392f
Chayti Saha <chaytisaha98@gmail.com>
2022-01-04 13:28:15 +0600

    connected with db

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iea9q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("coffeTime");
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Coffee Time Website!')
+})
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`)
+})
